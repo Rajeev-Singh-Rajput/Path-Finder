97a90b6f1bf144992c301ba0dabee3a37f42f222
Rajeev Singh Rajput <ssrajput2244@gmail.com>
2025-08-25 08:36:27 +0530

    Added Tailwind CSS setup

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/path-finder/',
+  base: '/Path-Finder/',
   plugins: [
     tailwindcss(),
     react(),
